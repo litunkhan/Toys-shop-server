e0937a894c1ae510f36a371d8e628ae6e79e73fd
litunkhan <litunkhan6@gmail.com>
2023-05-18 10:48:34 +0600

    mongoconnect

--- index.js
+++ index.js
@@ -9,11 +9,42 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6scxok5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    app.get('/',(req,res)=>{
     res.send('Start server')
 })
 app.listen(port,(req,res)=>{
     console.log('Server running Succesgully',)
 })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
