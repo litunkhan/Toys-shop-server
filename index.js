846ac5ad000a764d8e5d439678800e4561dd862a
litunkhan <litunkhan6@gmail.com>
2023-05-19 11:45:17 +0600

    postmethod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -11,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6scxok5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +29,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const toysCollections = client.db('toysdb').collection('toycollection')
+
+
+    app.post('/toys', async(req,res)=>{
+      const booking = req.body
+      const result = await toysCollections.insertOne(booking)
+      res.send(result)
+  })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
